6f4ced045ed4946a48cb7d6d113045a9641f5355
Ben Evans <benjamin.john.evans@gmail.com>
2012-07-13 07:54:11 -0700

    Do this commit before refactor

--- src/fix8on/DMAManager.java
+++ src/fix8on/DMAManager.java
@@ -1,6 +1,11 @@
 package fix8on;
 
+import java.util.List;
+
+import org.codehaus.jackson.JsonNode;
+
 import quickfix.Application;
+import quickfix.DefaultMessageFactory;
 import quickfix.DoNotSend;
 import quickfix.FieldNotFound;
 import quickfix.IncorrectDataFormat;
@@ -8,9 +13,22 @@ import quickfix.IncorrectTagValue;
 import quickfix.Message;
 import quickfix.RejectLogon;
 import quickfix.SessionID;
+import quickfix.SessionSettings;
 import quickfix.UnsupportedMessageType;
 
+/**
+ * This appliaction only deals with a single version of FIX - FIX 4.4 for simplicity.
+ * 
+ * @author boxcat
+ *
+ */
 public class DMAManager implements Application {
+
+    private DefaultMessageFactory messageFactory = new DefaultMessageFactory();
+	
+	public DMAManager(SessionSettings settings, List<JsonNode> clientCfgs) {
+		// TODO Auto-generated constructor stub
+	}
 
 	@Override
 	public void fromAdmin(Message arg0, SessionID arg1) throws FieldNotFound,
--- src/fix8on/Main.java
+++ src/fix8on/Main.java
@@ -1,28 +1,107 @@
 package fix8on;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.soap.MessageFactory;
+
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.JsonProcessingException;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import quickfix.Application;
+import quickfix.ConfigError;
+import quickfix.DefaultMessageFactory;
+import quickfix.FileStoreFactory;
+import quickfix.LogFactory;
+import quickfix.MessageStoreFactory;
+import quickfix.RuntimeError;
+import quickfix.ScreenLogFactory;
+import quickfix.SessionSettings;
+import quickfix.SocketAcceptor;
+
 public class Main {
 
-	private void init(String dirStr) {
+	private SocketAcceptor acceptor;
+	private Application app;
+	
+	private void init(String dirStr) throws ConfigError {
 		// Convert to a Path
 		
 		// Find all JSON objects
+		List<File> clientFiles = new ArrayList<>();
 		
 		// Find main config
+		JsonNode primaryCfg = createJsonObject(null);
 		
 		// Handle client configs
+		List<JsonNode> clientCfgs = clientFiles.map(f -> createJsonObject(f)).into(new ArrayList<JsonNode>());
+				
+		// Configure up the acceptor
+		SessionSettings settings = createSessionSettings(primaryCfg);
+		app = new DMAManager(settings, clientCfgs);
+        MessageStoreFactory msgStoreFactory = new FileStoreFactory(settings);
+        LogFactory logFactory = new ScreenLogFactory(true, true, true);
+        DefaultMessageFactory msgFactory = new DefaultMessageFactory();
+
+        acceptor = new SocketAcceptor(app, msgStoreFactory, settings,
+                                      logFactory, msgFactory);
+		
 	}
 	
+	private static JsonNode createJsonObject(File f) throws ConfigError {
+		String mainCfgStr = "";
+		
+		JsonNode primaryCfg;
+		try {
+			ObjectMapper mapper = new ObjectMapper();
+			primaryCfg = mapper.readTree(mainCfgStr);
+		} catch (IOException iox) {
+			throw new ConfigError(iox);
+		}
+
+		return primaryCfg;
+	}
+	
+	private SessionSettings createSessionSettings(JsonNode primaryCfg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 	private void run() {
 		
 	}
 	
 	/**
 	 * @param args
+	 * @throws ConfigError 
+	 * @throws RuntimeError 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws RuntimeError, ConfigError {
 		Main m = new Main();
 		m.init(args[0]);
+		m.start();
 		m.run();
+		m.stop();
+	}
+
+	/**
+	 * Now fully initialized, this method is used to start accepting connections
+	 * 
+	 * @throws RuntimeError
+	 * @throws ConfigError
+	 */
+	private void start() throws RuntimeError, ConfigError {
+		acceptor.start();
+	}
+	
+	/**
+	 * Cleanup method
+	 */
+	private void stop() {
+		acceptor.stop();
 	}
 
 }
